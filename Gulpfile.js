bd0f425a5eba9deebc4b0945e3095b59d6376aa3
samuel <samuelsilvawb@gmail.com>
2017-05-18 16:28:13 -0300

    configurando arquivos do gulp

---
+++ Gulpfile.js
@@ -0,0 +1,30 @@
+
+const gulp = require('gulp' );
+const jshint = require('gulp-jshint');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+ 
+const filesJs = './js/*.js';
+
+gulp.task('lint', function(){
+    gulp.src(filesJs)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('dist', function() {
+    gulp.src(filesJs)
+    .pipe(concat('./dist'))
+    .pipe(rename('dist.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', function () {
+    console.log('hello world');
+    gulp.run('lint', 'dist');
+    gulp.watch(files, function(evt) {
+        gulp.run('lint', 'dist');
+    });
+});

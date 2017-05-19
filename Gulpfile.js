ff508de69c090351f127f2061d494ece62215f4b
samuel <samuelsilvawb@gmail.com>
2017-05-19 16:17:57 -0300

    adicionando novas regras no gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,16 +5,30 @@ const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
 const babel = require('gulp-babel');
+const clean = require('gulp-clean');
  
-const filesJs = './js/*.js';
+const filesJs = './dist/*.js';
 
-gulp.task('lint', function(){
+gulp.task('clean', () => {
+  return gulp.src('./dist/*', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('lint', () => {
     gulp.src(filesJs)
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
-gulp.task('dist', function() {
+gulp.task('babel', () => {
+    return gulp.src('./js/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('dist', () => {
     gulp.src(filesJs)
     .pipe(concat('./dist'))
     .pipe(rename('dist.min.js'))
@@ -22,14 +36,4 @@ gulp.task('dist', function() {
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('babel', () => {
-    return gulp.src(filesJs)
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('default', function () {
-   gulp.watch(filesJs, ['lint', 'dist']);
-});
+gulp.task('default', ['clean', 'babel', 'dist']);
